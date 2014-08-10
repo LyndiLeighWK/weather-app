cb1b4646591af4f8840cc74ce16f530cd8118ccb
LyndiLeighWK <lyndi.weener.kuiper@gmail.com>
2014-08-10 12:06:02 -0400

    Added temp to script and view files

--- weather.rb
+++ weather.rb
@@ -6,12 +6,18 @@ def get_weather(location)
 	client.lookup_by_location(location).condition['text']
 end
 
+def get_temp(location)
+	client = Weatherman::Client.new
+	client.lookup_by_location(location).condition['temp']
+end
+
 get '/' do
 	erb :home
 end
 post '/weather' do
  	@post = params[:post]['location']
  	@weather = get_weather(@post).downcase
+ 	@temp = ((get_temp(@post) * 9)/5) + 32
 
  	"#{@weather}"
 
@@ -34,3 +40,9 @@ post '/weather' do
  	end
 end
 
+#next steps
+#add temp method and post data to view pages
+#create div for temp and weather condition and center vertically on page
+#stylize view for default page
+#stylize H1
+#stylize home page
